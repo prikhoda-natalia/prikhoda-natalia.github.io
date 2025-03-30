c6668fb6166a89d646a5716c9cb02cb3ded03f9a
prikhoda-natalia <prikhoda.natalia.webdev@gmail.com>
2025-03-30 17:46:56 +0000

    Deploying to gh-pages from @ prikhoda-natalia/prikhoda-natalia.github.io@c74d235fed137dec313055dea0c0eba64dfbd913 🚀

---
+++ assets/index-DVY1OieZ.js
@@ -0,0 +1,1 @@
+import{l as n,p as o}from"./chunk-XJI4KG32-DNwPZn9b.js";const a={this:"_this_1cux5_6"},r=({href:s,target:t="_blank",value:i})=>n.jsx(o,{className:a.this,to:s,target:t,children:i});export{r as L};
---
+++ assets/with-props-C2wod37L.js
@@ -0,0 +1,1 @@
+import{w as p,x as t,y as e,z as o,r as s,A as u}from"./chunk-XJI4KG32-DNwPZn9b.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
