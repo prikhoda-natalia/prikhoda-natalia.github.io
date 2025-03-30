c98946577715fb2d57763fa098747d618c6db54b
Natalia P. <prikhoda-natalia@users.noreply.github.com>
2025-03-30 15:45:21 +0100

    remove repo name from the base in vite's config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/prikhoda-natalia.github.io/',
+  base: '/',
   plugins: [reactRouter(), tsconfigPaths()],
   css: {
     preprocessorOptions: {
